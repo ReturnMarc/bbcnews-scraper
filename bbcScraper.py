32318299de85c900a3a173a8574eb08c9b00039e
md Ibrahim <ibrahimcoxs11@gmail.com>
2020-06-18 04:06:54 +0600

    Saved news to a JOSN file

--- bbcScraper.py
+++ bbcScraper.py
@@ -1,79 +1,81 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 root_url = 'https://www.bbc.com'
-response = requests.get(root_url + "/news")
+response = requests.get(root_url+"/news")
 doc = BeautifulSoup(response.text, 'html.parser')
 
-
-def find_bbc_article(url):
+def findBBCArticle(url):
     print(url)
-    response_article = requests.get(url)
-    soup = BeautifulSoup(response_article.text, 'html.parser')
-
+    responseArticle = requests.get(url)
+    soup = BeautifulSoup(responseArticle.text, 'html.parser')
+    
     body = soup.find(property="articleBody")
     description = [p.text for p in body.find_all("p")]
     if description:
         description = '\n'.join(description)
-
-    try:
-        img_url = soup.find('img', {'class': "js-image-replace"}).get('src')
-        # if imgUrl:
+    
+    try : 
+        imgUrl = soup.find('img',{'class':"js-image-replace"}).get('src')
+        #if imgUrl:
         #    print(imgUrl)
     except:
         print("No Image")
-    # imgUrl = 'test'
-
+    #imgUrl = 'test'
+    
     time = soup.find(class_="date").attrs['data-seconds']
-
-    # if time:
+    
+    
+    #if time:
     #    print(time)
-    # time = "fdsdf"
-    return description, img_url, time
+    #time = "fdsdf"
+    return description,imgUrl,time
+
+def BBCScraper():
+    
+    news_list = []
+    newsAll = doc.find_all('div', { 'class': 'gs-c-promo' })
+    for news in newsAll:
+        headline = news.find('h3')
+        link = news.find('a')
 
 
-def bbc_scraper():
-    stories_list = []
-    stories = doc.find_all('div', {'class': 'gs-c-promo'})
-    for story in stories:
-        headline = story.find('h3')
-        link = story.find('a')
+        articleUrl = root_url+link['href']
 
-        article_url = root_url + link['href']
+
 
         try:
-            description, imgUrl, time = find_bbc_article(article_url)
-
-            '''
-            story_dict = {
-                'headline'    : headline.text,
-                'description' : description,
-                'url'         : articleUrl,
-                'urlToImage'  : imgUrl,
-                'publishedAt' : time
-            } 
-            '''
-
-            story_dict = {
-                             "source": {
-                                 "id": link['href'],
-                                 "name": "BBC"
-                             },
-                             "author": "Null",
-                             "title": headline.text,
-                             "description": description,
-                             "url": article_url,
-                             "urlToImage": imgUrl,
-                             "publishedAt": time
-                         },
-
+            description,imgUrl,time = findBBCArticle(articleUrl)
+            
+            
+            article = {
+                  "source": {
+                    "id": link['href'],
+                    "name": "BBC"
+                    },
+                    "author": "Null",
+                    "title": headline.text,
+                    "description": description,
+                    "url": articleUrl,
+                    "urlToImage": imgUrl,
+                    "publishedAt": time
+                }
+            
             # Add the dict to our list
-            stories_list.append(story_dict)
+            news_list.append(article)
 
         except:
             print("Url error")
 
-    print(stories_list)
 
 
-bbc_scraper()
+    #print(stories_list)
+
+
+    with open('bbcNews.json', 'w', encoding='utf-8') as file:
+        json.dump(news_list , file, ensure_ascii=False, indent=4)
+
+    
+
+BBCScraper()
