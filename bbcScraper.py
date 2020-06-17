feeaf5552d89530e03b79c5fb5b281dd8bc7e0c2
md Ibrahim <ibrahimcoxs11@gmail.com>
2020-06-18 01:05:35 +0600

    Added BBC Scraper

---
+++ bbcScraper.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+
+root_url = 'https://www.bbc.com'
+response = requests.get(root_url+"/news")
+doc = BeautifulSoup(response.text, 'html.parser')
+
+def findBBCArticle(url):
+    print(url)
+    responseArticle = requests.get(url)
+    soup = BeautifulSoup(responseArticle.text, 'html.parser')
+    
+    body = soup.find(property="articleBody")
+    description = [p.text for p in body.find_all("p")]
+    if description:
+        description = '\n'.join(description)
+    
+    try : 
+        imgUrl = soup.find('img',{'class':"js-image-replace"}).get('src')
+        #if imgUrl:
+        #    print(imgUrl)
+    except:
+        print("No Image")
+    #imgUrl = 'test'
+    
+    time = soup.find(class_="date").attrs['data-seconds']
+    
+    
+    #if time:
+    #    print(time)
+    #time = "fdsdf"
+    return description,imgUrl,time
+
+def BBCScraper():
+
+    stories_list = []
+    stories = doc.find_all('div', { 'class': 'gs-c-promo' })
+    for story in stories:
+        headline = story.find('h3')
+        link = story.find('a')
+
+
+        articleUrl = root_url+link['href']
+
+
+
+        try:
+            description,imgUrl,time = findBBCArticle(articleUrl)
+            
+            '''
+            story_dict = {
+                'headline'    : headline.text,
+                'description' : description,
+                'url'         : articleUrl,
+                'urlToImage'  : imgUrl,
+                'publishedAt' : time
+            } 
+            '''
+            
+            story_dict = {
+                  "source": {
+                    "id": link['href'],
+                    "name": "BBC"
+                    },
+                    "author": "Null",
+                    "title": headline.text,
+                    "description": description,
+                    "url": articleUrl,
+                    "urlToImage": imgUrl,
+                    "publishedAt": time
+                },
+            
+            # Add the dict to our list
+            stories_list.append(story_dict)
+
+        except:
+            print("Url error")
+
+
+    print(stories_list)
+
+BBCScraper()
